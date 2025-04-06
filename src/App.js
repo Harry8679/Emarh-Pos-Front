30f1ff71c4646291c3b5f4df42674d61e299b881
Harry MacCode <emarh.harry.code@gmail.com>
2025-04-07 01:08:25 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Items from './pages/Items';
+// Tu pourras ajouter Bills, Customers, etc. aussi ici
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/items' element={<Items />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="flex">
+      {/* Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} />
+
+      {/* Main layout */}
+      <div className="flex-1 md:ml-64">
+        <Header toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} />
+        <main className="p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/items" element={<Items />} />
+            {/* Tu peux ajouter plus de routes ici comme : */}
+            {/* <Route path="/bills" element={<Bills />} /> */}
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 }
