b262dfb40deef11e924a00c6aa6168e3e5dbe9c9
Harry MacCode <emarh.harry.code@gmail.com>
2025-04-07 00:50:06 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Items from './pages/Items';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/items' element={<Items />} />
           <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
